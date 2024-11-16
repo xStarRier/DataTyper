3bfeb0bbe077501298b67e5df0c6475ee4eaeac3
xStarRier <stephanniequynh@gmail.com>
2024-11-16 19:41:23 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿int number = 5; -Intergrer, heltall.Kan lagre tall mellom 2,147,483,648 til 2,147,483,647.
+double number = 5.99; -Floating, kommatall.Kan holde opp til 15 decimal.
+
+Talltyper er hovedsakelig delt it to typer 
+- integrer types som er heltall (eks 123 eller 345). Her kan vi bruke int eller long, avhenging av verdien.
+
+-Floating point types er kommentar tall, med en eller fler desimaler. Her kan vi bruke float eller double. Merk at vi skriver "F" eller "D" bak tallet, avhenging av hvilken
+datatyper man bruker.
+
+char letter = "D"; -Character, holder en karakter eller bokstav. Bruker disse '';
+string text = "Hei"; -String, ord eller text. Bruker disse " ";
+bool isTrue = true; -Boolean, true/false statement.
+Var input = Console.ReadLine(); -Variabel, variarende innhold. Denne brukes hvis det ikke er en spesifik datatype. Eks når man bruker Console.ReadLine(), hvor man da ikke vet
+om man skriver inn et tall eller en tekst.
